6eb473971b444ddaee49ab341a3826cdc248a7f8
Novatux <nathanael.courant@laposte.net>
2016-01-02 19:08:22 +0100

    Add timeouts in run_tests_exec.sh

--- run_tests_exec.sh
+++ run_tests_exec.sh
@@ -17,12 +17,16 @@ for f in tests/exec/*.scala; do
     if (( $result == 0 )) ; then
         gcc $nm.s -o $nm
         if (( $? == 0 )) ; then
-          if $nm > out; then
+          timeout 1s $nm > out;
+          result=$?
+          if (( $result == 0 )) ; then
             if cmp --quiet out $nm.out; then
               score=`expr $score + 1`;
             else
               echo "  ECHEC : mauvaise sortie pour $f"
             fi
+          elif (( $result == 124 )) ; then
+            echo "  ECHEC : l'éxécution de $f a dépassé le temps imparti"
           else
             echo "  ECHEC de l'éxécution pour $f"
           fi
@@ -52,8 +56,12 @@ for f in tests/exec-fail/*.scala; do
     if (( $result == 0 )) ; then
         gcc $nm.s -o $nm
         if (( $? == 0 )) ; then
-          if $nm > out; then
+          timeout 1s $nm > out;
+          result=$?
+          if (( $result == 0 )) ; then
             echo "  ECHEC : l'éxécution de $f devrait échouer"
+          elif (( $result == 124 )) ; then
+            echo "  ECHEC : l'éxécution de $f a dépassé le temps imparti"
           else
             score=`expr $score + 1`;
           fi
